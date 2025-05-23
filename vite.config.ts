acb1a7363d151b0d32ff47e6e38c75a49295e6ac
Forester19 <vdvoreckij4@gmail.com>
2025-05-23 20:26:45 +0300

    CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/plane_demo_2/', // Replace 'planes' with your repository name
+  base: '/',
   build: {
     sourcemap: true,
     minify: 'terser',
